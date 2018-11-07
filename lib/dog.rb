a1eecbcd6f9b47d4512c3657044ae93e696886ac
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-07 20:02:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+  # make sure that this is capitalized 
+  
+end 
+
+fido = Dog.new 
+
+snoopy = Dog.new 
+
+lassie = Dog.new 
+
+# by adding the = you have a local variable 
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person 
+end 
+
+adele_goldberg = Person.new 
+
+alan_kay = Person.new
+
+# By adding .new you now have a new local variable- person
